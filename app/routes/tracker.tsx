37d1c306a5b4e585b0bd610a69108b31afde37bb
Mehdi Salemi <mehdisalemi@Mehdis-MacBook-Air.local>
2024-05-02 21:15:44 -0400

    Add starter dashboard

--- app/routes/tracker.tsx
+++ app/routes/tracker.tsx
@@ -1,5 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Calendar } from "../master-components/Calendar/Calendar";
+import { User } from "lucide-react";
+import { UserTracker } from "../components/UserTracker/UserTracker";
 
 export const meta: MetaFunction = () => {
   return [
@@ -14,12 +16,18 @@ export const meta: MetaFunction = () => {
 
 export default function TrackerPage() {
   return (
-    <>
+    <Layout>
+      <UserTracker />
+
       <div className="container flex justify-center px-4 md:px-6 py-8 ">
         <div className="flex flex-col items-center space-y-4 text-center p-4 md:w-1/2">
           <Calendar />
         </div>
       </div>
-    </>
+    </Layout>
   );
 }
+
+const Layout = ({ children }) => {
+  return <div className="flex flex-col px-16 py-8">{children}</div>;
+};
