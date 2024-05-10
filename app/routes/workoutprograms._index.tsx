95bbe7928ea7e9425a0da172ddb11d5a6fe85412
Mehdi Salemi <mehdisalemi@Mehdis-MacBook-Air.local>
2024-05-10 07:02:43 -0400

    nits

--- app/routes/workoutprograms._index.tsx
+++ app/routes/workoutprograms._index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import { WORKOUT_PROGRAMS_EXAMPLE } from "../lib/mehdi_bench_template";
+import { Search } from "../master-components/shadcn/search";
 
 import {
   Table,
@@ -11,6 +12,12 @@ import {
   TableHeader,
   TableRow,
 } from "../master-components/shadcn/table";
+
+import {
+  Layout,
+  LayoutBody,
+  LayoutHeader,
+} from "../master-components/shadcn/layout";
 
 import { Outlet, Link } from "@remix-run/react";
 
@@ -29,31 +36,28 @@ export default function WorkoutProgramsPage() {
   console.log(WORKOUT_PROGRAMS_EXAMPLE);
   return (
     <Layout>
-      <div className="text-center flex flex-col md:px-16 md:py-16">
-        <h1 className="text-5xl font-bold mb-4 px-8">
-          Explore our{" "}
-          <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-            workout program
-          </span>{" "}
-          library.
-        </h1>
-        <p className="mb-4 text-md opacity-80 font-light px-4">
-          Discover workout programs tailored to your goals. Whether you're
-          looking to build muscle, lose weight, or improve your overall fitness,
-          we have a program for you.
-        </p>
-        <div>
-          <WorkoutProgramsTable />
+      <LayoutHeader>
+        <div className="text-center flex flex-col items-center md:px-16 md:py-8">
+          <h1 className="text-5xl font-bold mb-4 px-8">
+            Explore our{" "}
+            <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+              workout program
+            </span>{" "}
+            library.
+          </h1>
+          <p className="mb-4 text-md opacity-80 font-light px-4">
+            Discover workout programs tailored to your goals. Whether you're
+            looking to build muscle, lose weight, or improve your overall
+            fitness, we have a program for you.
+          </p>
         </div>
-        <Outlet />
-      </div>
+      </LayoutHeader>
+      <LayoutBody>
+        <WorkoutProgramsSearchTable />
+      </LayoutBody>
     </Layout>
   );
 }
-
-const Layout = ({ children }) => {
-  return <div className="flex flex-col px-16 py-8">{children}</div>;
-};
 
 const invoices = [
   {
@@ -65,42 +69,52 @@ const invoices = [
     author: "MedxMan",
   },
 ];
-const WorkoutProgramsTable = () => {
+
+const WorkoutProgramsSearchTable = () => {
   console.log("go gere");
+
   return (
-    <Table>
-      <TableCaption>A list of workout programs.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Workout Program</TableHead>
-          <TableHead>Author</TableHead>
-          <TableHead>Workouts per Week</TableHead>
-          <TableHead className="">Number of Weeks</TableHead>
-          <TableHead className="text-right">View</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {invoices.map((invoice) => (
-          <TableRow key={invoice.id}>
-            <TableCell className="font-medium">
-              {invoice.workoutProgramName}
-            </TableCell>
-            <TableCell>{invoice.author}</TableCell>
-            <TableCell>{invoice.workoutsPerWeek}</TableCell>
-            <TableCell>{invoice.numberOfWeeks}</TableCell>
-            <TableCell>{invoice.numberOfWeeks}</TableCell>
-            <TableCell className="text-right link">
-              <Link to={invoice.url}>View</Link>
-            </TableCell>
+    <>
+      <div className="flex flex-col items-center">
+        <Search />
+      </div>
+      <Table>
+        <TableCaption>A list of workout programs.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[200px]">Workout Program</TableHead>
+            <TableHead>Author</TableHead>
+            <TableHead className="text-center">Workouts per Week</TableHead>
+            <TableHead className="text-center">Number of Weeks</TableHead>
+            <TableHead className="text-right">View</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-      {/* <TableFooter>
+        </TableHeader>
+        <TableBody>
+          {invoices.map((invoice) => (
+            <TableRow key={invoice.id}>
+              <TableCell className="font-medium">
+                {invoice.workoutProgramName}
+              </TableCell>
+              <TableCell>{invoice.author}</TableCell>
+              <TableCell className="text-center">
+                {invoice.workoutsPerWeek}
+              </TableCell>
+              <TableCell className="text-center">
+                {invoice.numberOfWeeks}
+              </TableCell>
+              <TableCell className="text-right link">
+                <Link to={invoice.url}>View</Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        {/* <TableFooter>
         <TableRow>
           <TableCell colSpan={3}>Total</TableCell>
           <TableCell className="text-right">$2,500.00</TableCell>
         </TableRow>
       </TableFooter> */}
-    </Table>
+      </Table>
+    </>
   );
 };
