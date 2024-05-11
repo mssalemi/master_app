ac1f424b54e694e6fd0c819201e2703935b63772
Mehdi Salemi <mehdisalemi@Mehdis-MacBook-Air.local>
2024-05-10 22:44:33 -0400

    profile pager

--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -1,5 +1,13 @@
 import type { MetaFunction } from "@remix-run/node";
 
+import {
+  Avatar,
+  AvatarImage,
+  AvatarFallback,
+} from "app/master-components/shadcn/avatar";
+
+import { Badge } from "app/master-components/shadcn/badge";
+
 // Metadata for the settings page
 export const meta: MetaFunction = () => {
   return [
@@ -11,6 +19,38 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const Props = {
+  user: {
+    username: "medxman",
+    firstName: "Med",
+    lastName: "Xman",
+    email: "medxman@strengthtrainning.ai.com",
+  },
+};
+
 export default function ProfilePage() {
-  return <>Profile</>;
+  return (
+    <Layout>
+      <h2 className="text-2xl font-bold">
+        {Props.user.firstName}
+        {"'s"} Profile
+      </h2>
+      <Avatar className="w-20 h-20">
+        <AvatarImage
+          src="https://i.postimg.cc/Kvp221Xn/IMG-2673.jpg"
+          alt="@medxman"
+        />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <Badge>Edit</Badge>
+    </Layout>
+  );
 }
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex flex-col px-16 py-8 gap-4 justify-center items-center">
+      {children}
+    </div>
+  );
+};
