f03a232226bef4d5689652311b46e77279b1a388
Mehdi Salemi <mehdisalemi@Mehdis-MacBook-Air.local>
2024-05-12 15:38:09 -0400

    basic done

--- app/components/UserTracker/tabs/AnalyticsTab.tsx
+++ app/components/UserTracker/tabs/AnalyticsTab.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import ComingSoon from "~/components/ComingSoon/CoomingSoon";
 
 export function AnalyticsTab() {
-  return <div className="space-y-4">Coming Soon</div>;
+  return (
+    <div className="space-y-4 px-16 py-8">
+      <ComingSoon />
+    </div>
+  );
 }
--- app/routes/workoutprograms.$workoutProgramId.tsx
+++ app/routes/workoutprograms.$workoutProgramId.tsx
@@ -1,10 +1,14 @@
+import { Link } from "@remix-run/react";
 import { WorkoutProgramInfo } from "../components/WorkoutProgram/Info";
 import React from "react";
 
 function WorkoutProgramPage() {
   return (
     <Layout>
-      <div className="container flex flex py-8">
+      <div className="container flex flex-col py-8">
+        <Link to="/workoutprograms" className="text-blue-500">
+          <span className="ml-2">All Workout Programs</span>
+        </Link>
         <div className="flex flex-col space-y-4 p-4">
           <WorkoutProgramInfo />
         </div>
